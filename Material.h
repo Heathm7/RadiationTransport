fd36662a3cc3f1958817a1ccdbc4088a8482a77e
Heath MercerHeath <heath.mercer3@gmail.com>
2026-01-16 16:12:30 -0600

    Added material struct

--- Material.h
+++ Material.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <string>
+
+struct Material {
+	std::string name;		// Name of the material (eg. Lead, Water)
+	double thickness;		// Thickness in cm
+	double absorptionProb;  // Probability of absorption per step (0 <= p <= 1)
+
+	Material() : name(""), thickness(1.0), absorptionProb(0.01) {}
+	Material(const std::string& n, double t, double p)
+		: name(n), thickness(t), absorptionProb(p) {}
+					
+	// I(x) = I_0e^mux ~ Monte Carlo average of particle survival
+};
